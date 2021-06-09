ed2f3f85e828e4a25d41ed5052d2e75c5b93a81a
Envy <envy15@mail.ru>
2021-06-09 13:15:04 +0300

    add base url class

--- src/_app.py
+++ src/_app.py
@@ -5,6 +5,3 @@ from flask import Flask
 __all__ = ['app']
 
 app: Final[Flask] = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run()
---
+++ src/urls/_base_url.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from http import HTTPStatus
+
+from flask import Request, Response, request as flask_request
+
+from src import app
+
+__all__ = ['BaseUrl']
+
+
+class BaseUrl(ABC):
+    def __init__(self):
+        app.add_url_rule(
+            rule=self.url,
+            endpoint=self.__class__.__name__,
+            view_func=self._index,
+            methods=['GET', 'POST']
+        )
+
+    def _index(self) -> Response:
+        response: Response
+        request: Request = flask_request
+        # noinspection PyBroadException
+        try:
+            method = request.method.upper()
+            if method == 'GET':
+                response = self.get(request)
+            else:  # POST
+                response = self.post(request)
+        except Exception:
+            response = app.make_response('Error')
+            response.status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+        return response
+
+    @property
+    @abstractmethod
+    def url(self) -> str:
+        raise NotImplementedError
+
+    def get(self, request: Request) -> Response:
+        return app.make_response('Not supported')
+
+    def post(self, request: Request) -> Response:
+        return app.make_response('Not supported')
