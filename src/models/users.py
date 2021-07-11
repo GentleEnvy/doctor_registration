26e783d07d4d6f718964d7c75799ca977d3b50e2
Envy <envy15@mail.ru>
2021-07-11 12:16:17 +0300

    update User models

--- src/models/users.py
+++ src/models/users.py
@@ -1,24 +1,43 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from src.db import db
 
 __all__ = ['Patient', 'Doctor', 'Admin']
 
 
-class BaseUser:
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-    def __repr__(self):
-        return f'<{self.__class__.__name__} ({self.username})>'
+    patient = db.relationship('Patient', uselist=False, back_populates='user')
+    doctor = db.relationship('Doctor', uselist=False, back_populates='user')
+    admin = db.relationship('Admin', uselist=False, back_populates='user')
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
-class Patient(BaseUser, db.Model):
-    pass
+class Patient(User, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user = db.relationship('User', back_populates='patient')
 
 
-class Doctor(BaseUser, db.Model):
-    pass
+class Doctor(User, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user = db.relationship('User', back_populates='doctor')
 
 
-class Admin(BaseUser, db.Model):
-    pass
+class Admin(User, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user = db.relationship('User', back_populates='admin')
