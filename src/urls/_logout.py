57d97ec34d702bf4ddd420e70c4b877886f3c665
Envy <envy15@mail.ru>
2021-07-11 13:18:24 +0300

    add logout

---
+++ src/urls/_logout.py
@@ -0,0 +1,14 @@
+from flask import redirect
+from flask_login import logout_user
+
+from src.urls._base import BaseUrl
+
+__all__ = ['LogoutUrl']
+
+
+class LogoutUrl(BaseUrl):
+    url = '/logout'
+
+    def get(self, request):
+        logout_user()
+        return redirect('/')
