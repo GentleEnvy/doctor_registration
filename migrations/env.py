4931333cf59b6f31d7bff5ed36c3a2e358ce1f88
Envy <envy15@mail.ru>
2021-07-13 18:32:37 +0300

    update User model

--- migrations/env.py
+++ migrations/env.py
@@ -24,7 +24,7 @@ config.set_main_option(
     'sqlalchemy.url',
     str(current_app.extensions['migrate'].db.get_engine().url).replace(
         '%', '%%'))
-target_metadata = current_app.extensions['migrate'].db._metadata
+target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- src/models/users.py
+++ src/models/users.py
@@ -8,6 +8,10 @@ __all__ = ['User', 'Patient', 'Doctor', 'Admin']
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=False)
+    mid_name = db.Column(db.String(100), nullable=True)
+    passport = db.Column(db.Integer, nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
