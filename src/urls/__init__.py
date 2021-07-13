cbf25b9843a837d758918895a39a73c681d17978
Envy <envy15@mail.ru>
2021-07-13 21:34:40 +0300

    add doctors url

--- src/urls/__init__.py
+++ src/urls/__init__.py
@@ -2,8 +2,10 @@ from src.urls._index import IndexUrl
 from src.urls._auth import AuthUrl
 from src.urls._register import RegisterUrl
 from src.urls._logout import LogoutUrl
+from src.urls._doctors import DoctorsUrl
 
 IndexUrl()
 AuthUrl()
 RegisterUrl()
 LogoutUrl()
+DoctorsUrl()
--- src/urls/_doctors.py
+++ src/urls/_doctors.py
@@ -3,6 +3,8 @@ from flask import render_template
 from src.models import Doctor
 from src.urls.base import BaseUrl
 
+__all__ = ['DoctorsUrl']
+
 
 class DoctorsUrl(BaseUrl):
     url = '/doctors'
