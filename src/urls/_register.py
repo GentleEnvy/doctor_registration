e9478c7aba3c3812ced067b66c3f90ae6fa6dc5d
Envy <envy15@mail.ru>
2021-07-14 19:54:56 +0300

    cleanup

--- src/urls/_register.py
+++ src/urls/_register.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, Request, Response
+from flask import render_template, redirect
 from flask_login import current_user, login_user
 
 from src.db import db
-from src.models import User
+from src.models import User, Patient
 from src.urls.base import BaseUrl
 
 __all__ = ['RegisterUrl']
@@ -24,11 +24,11 @@ class RegisterUrl(BaseUrl):
         if User.query.filter_by(username=username).first():
             return render_template('register.html', error_username=True, **form)
 
-        user = User(
+        patient = Patient(
             **(form | {'passport': int(form['passport'].replace(' ', ''))})
         )  # FIXME: add __init__
-        user.set_password(password)
-        db.session.add(user)
+        patient.set_password(password)
+        db.session.add(patient)
         db.session.commit()
-        login_user(user)
+        login_user(patient)
         return redirect('/')
