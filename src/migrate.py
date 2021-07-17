adc6f6ddeb5a687adf964ee86076b07535183cca
Envy <58393514+GentleEnvy@users.noreply.github.com>
2021-07-17 11:24:55 +0300

    Update migrate.py

--- src/migrate.py
+++ src/migrate.py
@@ -1,5 +1,3 @@
-from typing import Final
-
 from flask_migrate import Migrate
 
 from src.app import app
@@ -7,4 +5,4 @@ from src.db import db
 
 __all__ = ['migrate']
 
-migrate: Final[Migrate] = Migrate(app, db, render_as_batch=True)
+migrate = Migrate(app, db, render_as_batch=True)
