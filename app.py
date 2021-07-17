6d9a07cdc7d065d40dc4f486a56a03117d7b74fb
Envy <58393514+GentleEnvy@users.noreply.github.com>
2021-07-17 13:38:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from src.app import app
+import os
+
+from flask import Flask
+
+__all__ = ['app']
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or '__secret__'
+app.debug = bool(int(os.environ.get('DEBUG') or 0))
